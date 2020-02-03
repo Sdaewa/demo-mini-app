17d3e34588f1ae22dd507c690d6bca763c4119ab
Luis Ramirez <sdaewa22@gmail.com>
2020-02-03 13:11:50 +0000

    adding filter to renderMovies

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -3,53 +3,56 @@ searchBtn = document.getElementById('search-btn');
 
 const movies = [];
 
-const renderMovies = () => {
-    const movieList = document.getElementById('movie-list');
-    if (movies.length === 0) {
-        movieList.classList.remove('visible');
-        returnl
-    } else {
-        movieList.classList.add('visible');
-    }
-    movieList.innerHTML = ''; // not ideal
-    movies.forEach((movie) => {
-        const movieEl = document.createElement('li');
-        // movieEl.textContent = movie.info.title;
-        let text = movie.info.title + ' - ';
-        for (const key in movie.info) {
-            if (key !== 'title') {
-                text = text + `${key}: ${movie.info[key]}`
-            }
+const renderMovies = (filter = '') => {
+        const movieList = document.getElementById('movie-list');
+        if (movies.length === 0) {
+            movieList.classList.remove('visible');
+            returnl
+        } else {
+            movieList.classList.add('visible');
+        }
+        movieList.innerHTML = ''; // not ideal
+
+        const filteredMovies = !filter ? movies : movies.filter(;
+
+            movies.forEach((movie) => {
+                const movieEl = document.createElement('li');
+                // movieEl.textContent = movie.info.title;
+                let text = movie.info.title + ' - ';
+                for (const key in movie.info) {
+                    if (key !== 'title') {
+                        text = text + `${key}: ${movie.info[key]}`
+                    }
+                };
+                movieEl.textContent = text;
+                movieList.append(movieEl);
+            });
         };
-        movieEl.textContent = text;
-        movieList.append(movieEl);
-    });
-};
 
-const addMoviHandler = () => {
-    const title = document.getElementById('title').value;
-    const extraName = document.getElementById('extra-name').value;
-    const extraValue = document.getElementById('extra-value').value;
+        const addMoviHandler = () => {
+            const title = document.getElementById('title').value;
+            const extraName = document.getElementById('extra-name').value;
+            const extraValue = document.getElementById('extra-value').value;
 
-    if (title.trim() === '' || extraName.trim() === '' || extraValue.trim() === '') {
-        return;
-    }
-    const newMovie = {
-        info: {
-            title,
-            [extraName]: extraValue
-        },
-        id: Math.random()
-    };
-    movies.push(newMovie);
-    renderMovies();
-    // console.log(newMovie);
-};
+            if (title.trim() === '' || extraName.trim() === '' || extraValue.trim() === '') {
+                return;
+            }
+            const newMovie = {
+                info: {
+                    title,
+                    [extraName]: extraValue
+                },
+                id: Math.random()
+            };
+            movies.push(newMovie);
+            renderMovies();
+            // console.log(newMovie);
+        };
 
-const searchMovieHandler = () => {
-    const filterSearch = document.getElementById('filter-title').value;
-    renderMovies(filterSearch);
-};
+        const searchMovieHandler = () => {
+            const filterSearch = document.getElementById('filter-title').value;
+            renderMovies(filterSearch);
+        };
 
-addMovieBtn.addEventListener('click', addMoviHandler);
-searchBtn.addEventListener('click', searchMovieHandler);
+        addMovieBtn.addEventListener('click', addMoviHandler);
+        searchBtn.addEventListener('click', searchMovieHandler);
